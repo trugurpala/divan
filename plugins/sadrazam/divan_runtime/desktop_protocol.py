d0cc797832b8024199b5219e3949dc8a4436a34a
Uğur Pala <mail@ugurpala.com>
2026-08-07 18:39:24 +0300

    style(desktop): normalize protocol support imports

--- plugins/sadrazam/divan_runtime/desktop_protocol.py
+++ plugins/sadrazam/divan_runtime/desktop_protocol.py
@@ -6,13 +6,11 @@ from typing import Any
 from uuid import uuid4
 
 from .desktop_api import DesktopApi
-from .desktop_protocol_support import (
-    ProtocolValidationError,
-    error_response as _error,
-    ok_response as _ok,
-    optional_string as _optional_string,
-    required_string as _required_string,
-)
+from .desktop_protocol_support import ProtocolValidationError
+from .desktop_protocol_support import error_response as _error
+from .desktop_protocol_support import ok_response as _ok
+from .desktop_protocol_support import optional_string as _optional_string
+from .desktop_protocol_support import required_string as _required_string
 from .desktop_state import evidence_root, task_root
 from .execution_router import ExecutionRouter
 from .orchestrator import DivanOrchestrator
