76d93db699cbd37064c5605ff4ac30ee3ce6867a
Uğur Pala <mail@ugurpala.com>
2026-08-23 14:56:40 +0300

    feat(pusula): proxy web API to Django in development

--- incubator/pusula/web/vite.config.ts
+++ incubator/pusula/web/vite.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: false,
+      },
+    },
   },
   preview: {
     host: '127.0.0.1',
